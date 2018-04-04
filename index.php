c51c57332bcc18b16ef943c4723f6dbc54b4529b
Vincent JONQUAIS <jonquais.vincent@laposte.net>
2018-04-04 16:17:27 +0200

    test 2

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
+<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+
 <?php
 include "./includes/header.php";
+
+if (isset($_GET['page']) && $_GET['page'] != "") {
+
+    $page = $_GET['page'];
+
+}
+
+else {
+
+    $page = "home";
+
+}
+
+$page = "./includes/" . $page . ".inc.php";
+
+$incFiles = glob("./includes/*.inc.php");
+
+if (in_array($page, $incFiles)) {
+    include $page;
+}
+
+else {
+    include "./includes/home.inc.php";
+}
 
 include "./includes/footer.php";
 
