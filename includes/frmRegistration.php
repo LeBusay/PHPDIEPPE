cfda7de9853d010f89858601af95c4cfc1273df0
Vincent JONQUAIS <jonquais.vincent@laposte.net>
2018-04-05 09:41:47 +0200

    Import du formulaire "frmregistration"

---
+++ includes/frmRegistration.php
@@ -0,0 +1,23 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom : </label>
+        <input type="text" name="nom"/>
+    </div>
+    <div>
+        <label for="prenom">Prénom : </label>
+        <input type="text" name="prenom"/>
+    </div>
+    <div>
+        <label for="email">Mail : </label>
+        <input type="text" name="email"/>
+    </div>
+    <div>
+        <label for="motDePasse">Mot de passe : </label>
+        <input type="password" name="motDePasse"/>
+    </div>
+    <div>
+        <input type="reset" value="Effacer" />
+        <input type="submit" value="Envoyer" />
+    </div>
+    <input type="hidden" name="frmRegistration" />
+</form>
